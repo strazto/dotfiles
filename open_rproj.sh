0c2f1cbd3e592359bde027b1aad9723308382c42
Matthew Strasiotto <mstr3336@uni.sydney.edu.au>
2019-12-18 14:53:53 +1100

    Whitespace fixes to Rproj script

--- open_rproj.sh
+++ open_rproj.sh
@@ -1,6 +1,6 @@
 
 function Rproj_open {
-  xdg-open $(find . -maxdepth 1 -name "*.Rproj")>/dev/null 2>&1 &
+  xdg-open $(find . -maxdepth 1 -name "*.Rproj") > /dev/null 2>&1 &
 }
 
 alias Rproj=Rproj_open
