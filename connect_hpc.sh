cb651f3163422a62c96b38b31007cd3a097e374e
Matthew Strasiotto <mstr3336@uni.sydney.edu.au>
2020-05-29 12:11:43 +1000

    Update connect_hpc script

--- connect_hpc.sh
+++ connect_hpc.sh
@@ -2,23 +2,23 @@
 
 # For Ubuntu 18.04
 # Requires sshfs and openconnect
-# Requires UNIPASS and UNIKEY env vars to be set in
-# ~/.uni_auth
+# Requires UNIPASS and UNIKEY env vars to be set
+
 
 source ~/.uni_auth
 UNIKEY=$UNIKEY
 UNIPASS=$UNIPASS
 
-
-sudo -s echo "${UNIPASS}" | sudo -s openconnect -b -u $UNIKEY --passwd-on-stdin https://vpn.sydney.edu.au
+sudo -s echo "${UNIPASS}" | sudo -s openconnect -b -u $UNIKEY --passwd-on-stdin https://vpn.sydney.edu.au  
 
 echo "Finished connecting!"
 
 sleep 1
 
-#ssh -v -fN "${UNIKEY}@hpc.sydney.edu.au"
+#ssh -v -fN "${UNIKEY}@hpc.sydney.edu.au" 
 
 sudo -s sshfs "${UNIKEY}@hpc.sydney.edu.au:/project/STEMI/" /project/STEMI -o allow_other -o IdentityFile=~/.ssh/id_rsa -o reconnect,ServerAliveInterval=15,ServerAliveCountMax=3
 
-ssh -X "${UNIKEY}@hpc.sydney.edu.au"
+ssh -XYC "${UNIKEY}@hpc.sydney.edu.au"
 echo "Finsihed ssh-ing"
+bash
