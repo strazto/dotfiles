ca7b93a682a2c64525804343ea374c7e757aa2ee
Matthew Strasiotto <mstr3336@uni.sydney.edu.au>
2020-10-06 17:01:34 +1100

    Conditionally prepend include statement to ssh config file

---
+++ bootstrap/tools/ssh/add_include.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+append_or_not () {
+  local to_append='Include config.d/*'
+
+  local sshv="$(ssh -V 2>&1 | sed 's@^[^0-9]*\([0-9\.]\+\).*@\1@')"
+  
+  if (( "$(echo "$sshv < 7.3" | bc -l)" )); then
+    echo "SSH version $sshv < 7.3, doesnt support includes"
+    return 1
+  fi
+  
+  if [[ ! -e "$HOME/.ssh/config" ]]; then
+    touch "$HOME/.ssh/config"
+  fi
+  local linenums="$(sed -n "\,^${to_append},=" "$HOME/.ssh/config")"
+
+  if [[ ! -z "$linenums" ]]; then
+    echo "Found line @ $linenums"
+    return 2
+  fi
+  
+  sed -i "1i ${to_append}" "$HOME/.ssh/config"
+
+}
+
+append_or_not
