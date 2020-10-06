42765f8aa34dcd5e4e468d6cd8e74c47e66abb04
Matthew Strasiotto <mstr3336@uni.sydney.edu.au>
2020-10-06 17:43:47 +1100

    Improve output for ssh config update

--- bootstrap/tools/ssh/add_include.sh
+++ bootstrap/tools/ssh/add_include.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 
 append_or_not () {
-  local to_append='Include config.d/*'
+  echo "Update $HOME/.ssh/config :"
 
+  local to_append='Include config.d/*'
   local sshv="$(ssh -V 2>&1 | sed 's@^[^0-9]*\([0-9\.]\+\).*@\1@')"
   
+  echo " - $to_append"
+  
   if (( "$(echo "$sshv < 7.3" | bc -l)" )); then
-    echo "SSH version $sshv < 7.3, doesnt support includes"
+    echo "  - SSH version $sshv < 7.3, doesnt support includes"
     # TODO: Hint @ https://superuser.com/a/916583/881137
     # TODO: write a nice script for the above
     return 1
@@ -18,7 +21,7 @@ append_or_not () {
   local linenums="$(sed -n "\,^${to_append},=" "$HOME/.ssh/config")"
 
   if [[ ! -z "$linenums" ]]; then
-    echo "Found line @ $linenums"
+    echo "   - Already exists: lines #$linenums"
     return 0
   fi
   
