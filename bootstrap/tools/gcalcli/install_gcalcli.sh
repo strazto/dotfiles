65f2d7b9b095f9fb85a21c79b92f91165c2d3152
Matthew Strasiotto <39424834+matthewstrasiotto@users.noreply.github.com>
2021-03-04 20:41:47 +1100

    install to ~/.local if possible

--- bootstrap/tools/gcalcli/install_gcalcli.sh
+++ bootstrap/tools/gcalcli/install_gcalcli.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
-hash gcalcli 2>1 >/dev/null ||  python3 -m pip --user install gcalcli
+export USER_SITE=~/.local
+
+hash gcalcli 2>1 >/dev/null ||  python3 -m pip install --user gcalcli
 
