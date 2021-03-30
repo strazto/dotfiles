3c92f317d4f465008d48ce3c51134bb09154463e
Matthew Strasiotto <39424834+matthewstrasiotto@users.noreply.github.com>
2021-03-30 14:35:33 +1100

    update arrange script

--- bootstrap/envs/ubuntu/display_conf/bin/arrange_monitors.bash
+++ bootstrap/envs/ubuntu/display_conf/bin/arrange_monitors.bash
@@ -8,8 +8,8 @@ echo "Assigning MetaMode (Startup script)"
 nvidia-settings --assign CurrentMetaMode="\
   DP-5: nvidia-auto-select +0+0, \
   DP-1: nvidia-auto-select +0+1080, \
-  DP-3: nvidia-auto-select +1920+1080, \
-  HDMI-0: nvidia-auto-select +1920+0"
+  HDMI-0: nvidia-auto-select +1920+1080, \
+  DP-3: nvidia-auto-select +1920+0"
 
 
 
