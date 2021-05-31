b24e6748bd236454d543a673ef4dada6230486e4
Matthew Strasiotto <39424834+matthewstrasiotto@users.noreply.github.com>
2021-05-31 11:15:54 +1000

    wezterm config includes ssh domains

--- bootstrap/apps/wezterm/wezterm.lua
+++ bootstrap/apps/wezterm/wezterm.lua
@@ -1,9 +1,10 @@
 local wezterm = require 'wezterm'
+local ssh_domains = require 'conf_d.ssh_domains'
 return {
   font = wezterm.font_with_fallback({
     "Fira Code",
     "FiraCode Nerd Font Mono",
-  },
+  }),
   color_scheme = "Dark Pastel",
-  ),
+  ssh_domains = ssh_domains,
 }
