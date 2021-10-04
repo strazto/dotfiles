32be6886f6ad7d3016168b2c894784e182d94eca
Matthew Strasiotto <39424834+matthewstrasiotto@users.noreply.github.com>
2021-10-04 17:40:58 +1100

    Condiitonally include default prog config

--- apps/wezterm/wezterm.lua
+++ apps/wezterm/wezterm.lua
@@ -1,6 +1,13 @@
 local wezterm = require 'wezterm'
 local ssh_domains = require 'conf_d.ssh_domains'
-return {
+
+
+function file_exists(name)
+  local f=io.open(name,"r")
+  if f~=nil then io.close(f) return true else return false end
+end
+      
+config = {
   font = wezterm.font_with_fallback({
     "Fira Code",
     "FiraCode Nerd Font Mono",
@@ -8,3 +15,9 @@ return {
   color_scheme = "Dark Pastel",
   ssh_domains = ssh_domains,
 }
+
+if file_exists('conf_d/default_prog') then
+  config["default_prog"] = require 'conf_d.default_prog' 
+end
+
+return config
