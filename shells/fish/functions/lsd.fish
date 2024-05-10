c5d9eb31e53b53792903ec3dab6824430ae679ad
Matthew Strasiotto <39424834+matthewstrasiotto@users.noreply.github.com>
2024-05-10 14:22:17 +1000

    feat(fish.ls): Fix abbrs

--- shells/fish/functions/lsd.fish
+++ shells/fish/functions/lsd.fish
@@ -1,12 +1,12 @@
 if type -q lsd
     alias ls=lsd
-    abbr l lsd -l
-    abbr la lsd --almost-all
-    abbr lla lsd -l --almost-all
-    abbr lt lsd --tree
+    abbr --add l lsd -l
+    abbr --add la lsd --almost-all
+    abbr --add lla lsd -l --almost-all
+    abbr --add lt lsd --tree
 else
     alias ls='ls --color=auto'
-    abbr l ls -CF
-    abbr la ls -A
-    abbr ll ls -alF
+    abbr --add l ls -CF
+    abbr --add la ls -A
+    abbr --add ll ls -alF
 end
