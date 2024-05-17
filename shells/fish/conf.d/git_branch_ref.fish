f97eaca69343e4d908cac6ba1ad7d77daac30735
Matthew Strasiotto <39424834+matthewstrasiotto@users.noreply.github.com>
2024-05-17 16:48:10 +1000

    feat(fish.git): add alias for merge base

--- shells/fish/conf.d/git_branch_ref.fish
+++ shells/fish/conf.d/git_branch_ref.fish
@@ -1,2 +1,4 @@
 
 abbr --add git_branch_ref --position anywhere --set-cursor "$(git branch --show-current).$(git rev-parse HEAD | head -c 7)%"
+
+abbr --add git_branch_master --position anywhere --set-cursor "master.$(git rev-parse HEAD | head -c 7)%"
