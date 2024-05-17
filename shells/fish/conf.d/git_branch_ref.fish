5d60e5a0afb75b6234d4567ede19ca22d4abb640
Matthew Strasiotto <39424834+matthewstrasiotto@users.noreply.github.com>
2024-05-17 20:10:41 +1000

    fix(fish.git): Improve branch ref alias

--- shells/fish/conf.d/git_branch_ref.fish
+++ shells/fish/conf.d/git_branch_ref.fish
@@ -1,4 +1,12 @@
 
-abbr --add git_branch_ref --position anywhere --set-cursor "$(git branch --show-current).$(git rev-parse HEAD | head -c 7)%"
+function _git_branch_ref
+    echo "$(git branch --show-current).$(git rev-parse HEAD | head -c 7)%"
+end
 
-abbr --add git_branch_master --position anywhere --set-cursor "master.$(git rev-parse HEAD | head -c 7)%"
+abbr --add git_branch_ref --position anywhere --set-cursor --function _git_branch_ref
+
+function _git_branch_master
+    echo "master.$(git rev-parse HEAD | head -c 7)%"
+end
+
+abbr --add git_branch_master --position anywhere --set-cursor --function _git_branch_master
