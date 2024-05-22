edd4ba03564ec825dfe49ddf9d60877ef81df20e
Matthew Strasiotto <39424834+matthewstrasiotto@users.noreply.github.com>
2024-05-22 19:39:07 +1000

    feat(profiles.termux): install common utilities

---
+++ envs/termux/install_utils.bash
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# Maintain parity with /tools/linuxbrew/install_brew_utils.bash
+
+hash cargo 2>/dev/null || pkg install rust
+
+hash lsd 2>/dev/null || pkg install lsd
+
+hash fzf 2>/dev/null || pkg install fzf
+
+hash zoxide 2>/dev/null || pkg install zoxide
+
+hash tmux 2>/dev/null || pkg install tmux
+
+hash fish 2>/dev/null || pkg install fish
