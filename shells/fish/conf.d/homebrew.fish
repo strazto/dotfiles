146c7829ca47589526e32f266d64126a3c2d3fab
Matthew Strasiotto <39424834+matthewstrasiotto@users.noreply.github.com>
2024-05-28 15:30:32 +1000

    feat(fish.homebrew): Prepend homebrew to path

--- shells/fish/conf.d/homebrew.fish
+++ shells/fish/conf.d/homebrew.fish
@@ -9,7 +9,7 @@ else if test -d /opt/homebrew # MacOS
 	set -gx HOMEBREW_CELLAR "$HOMEBREW_PREFIX/Cellar"
 	set -gx HOMEBREW_REPOSITORY "$HOMEBREW_PREFIX/homebrew"
 end
-fish_add_path -gP "$HOMEBREW_PREFIX/bin" "$HOMEBREW_PREFIX/sbin";
+fish_add_path --move --prepend -gP "$HOMEBREW_PREFIX/bin" "$HOMEBREW_PREFIX/sbin";
 ! set -q MANPATH; and set MANPATH ''; set -gx MANPATH "$HOMEBREW_PREFIX/share/man" $MANPATH;
 ! set -q INFOPATH; and set INFOPATH ''; set -gx INFOPATH "$HOMEBREW_PREFIX/share/info" $INFOPATH;
 
