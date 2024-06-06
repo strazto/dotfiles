0f55ace1b8a886bff634bebbb616e6ae08522c17
Matthew Strasiotto <39424834+matthewstrasiotto@users.noreply.github.com>
2024-06-06 12:49:42 +1000

    feat(fish.docker): abbreviations resolve docker compose binaries

--- shells/fish/conf.d/docker.fish
+++ shells/fish/conf.d/docker.fish
@@ -1,4 +1,12 @@
-abbr --add dc docker-compose
+function _get_docker_binary
+    set dc docker-compose
 
-abbr --add dc-ro docker-compose -f docker-compose.yml -f docker-compose.override.yml -f docker-compose.readonly.yml
+    docker compose &> /dev/null && set dc "docker compose"
+    echo $dc
+end
 
+abbr --add dc (_get_docker_binary)
+
+abbr --add dc-ro (_get_docker_binary) -f docker-compose.yml -f docker-compose.override.yml -f docker-compose.readonly.yml
+
+abbr --add dc-e2e (_get_docker_binary) -f docker-compose.no-bind.yaml -f docker-compose.e2e.yaml
