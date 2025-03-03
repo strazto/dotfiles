583f952e4808e082492084337f0b2a534c0dba7f
Matthew Strasiotto <39424834+matthewstrasiotto@users.noreply.github.com>
2025-03-03 15:21:18 +1000

    feat(fish.docker): Add abbreviation for split compose stack

--- shells/fish/conf.d/docker.fish
+++ shells/fish/conf.d/docker.fish
@@ -13,3 +13,6 @@ abbr --add dc (_get_docker_binary)
 abbr --add dc-ro (_get_docker_binary) -f docker-compose.yml -f docker-compose.override.yml -f docker-compose.readonly.yml
 
 abbr --add dc-e2e (_get_docker_binary) -f docker-compose.no-bind.yaml -f docker-compose.e2e.yaml
+
+# Split media server stack
+abbr --add dc-split (_get_docker_binary) -f docker-compose.yml -f docker-compose.override.yml -f gateway/docker-compose.yml -f gateway/docker-compose.override.yml -f worker/docker-compose.yml -f worker/docker-compose.override.yml -f worker/docker-compose.readonly.yml
