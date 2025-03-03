570e7b5a9db4bfebfefed19c7e4432b8d93e5683
Matthew Strasiotto <39424834+matthewstrasiotto@users.noreply.github.com>
2025-03-03 15:29:56 +1000

    feat(fish.docker): Add abbreviation for extra compose projects

--- shells/fish/conf.d/docker.fish
+++ shells/fish/conf.d/docker.fish
@@ -12,6 +12,8 @@ abbr --add dc (_get_docker_binary)
 
 abbr --add dc-ro (_get_docker_binary) -f docker-compose.yml -f docker-compose.override.yml -f docker-compose.readonly.yml
 
+abbr --add dc-ex (_get_docker_binary) -f docker-compose.yml -f docker-compose.override.yml -f docker-compose.readonly.yml -f ../media_secondary/compose.yml
+
 abbr --add dc-e2e (_get_docker_binary) -f docker-compose.no-bind.yaml -f docker-compose.e2e.yaml
 
 # Split media server stack
