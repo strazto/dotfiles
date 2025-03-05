52f284ad3501aba153594b7d0fe0ad5be70d77b5
Matthew Strasiotto <39424834+matthewstrasiotto@users.noreply.github.com>
2025-03-05 13:35:16 +1000

    feat(fish.golang): setup gopath

--- shells/fish/conf.d/golang.fish
+++ shells/fish/conf.d/golang.fish
@@ -1,3 +1,7 @@
 if test -d /usr/local/go/bin
     fish_add_path --move --prepend --global --path /usr/local/go/bin
 end
+
+if test -d ~/go/bin
+    fish_add_path --move --prepend --global --path ~/go/bin
+end
