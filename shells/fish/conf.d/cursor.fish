68c14d6f0191b4a0e4fc9764fbe11a37d5839d5f
Matthew Strasiotto <39424834+strazto@users.noreply.github.com>
2025-09-29 10:17:17 +0000

    feat(fish.cursor): expand cursor detection for remote sessions

--- shells/fish/conf.d/cursor.fish
+++ shells/fish/conf.d/cursor.fish
@@ -2,7 +2,25 @@ if ! status is-interactive
     exit
 end
 
-if set -q CURSOR_TRACE_ID
-    abbr --add code cursor
+function is_cursor_session
+    # Heuristics: CURSOR_TRACE_ID or '.cursor-server' present in select env vars
+    if set -q CURSOR_TRACE_ID
+        return 0
+    end
+
+    for name in VSCODE_GIT_ASKPASS_MAIN VSCODE_GIT_ASKPASS_NODE NODE_OPTIONS BROWSER
+        set -l value (printenv $name 2>/dev/null)
+        if test -n "$value"; and string match -q '*/.cursor-server/*' -- $value
+            return 0
+        end
+    end
+
+    return 1
+end
+
+if is_cursor_session
+    if not abbr --query code
+        abbr --add code cursor
+    end
 end
 
