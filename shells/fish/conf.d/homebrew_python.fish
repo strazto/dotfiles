c87b52e8842ee7c45c73f9c14cde30b05f35674a
Matthew Strasiotto <39424834+matthewstrasiotto@users.noreply.github.com>
2025-10-01 22:43:47 +1000

    fix(fish.paths): fish_add_paths as part of config now sets global PATH not universal fish_user_paths

--- shells/fish/conf.d/homebrew_python.fish
+++ shells/fish/conf.d/homebrew_python.fish
@@ -1,4 +1,7 @@
 
 if test -d "$(brew --prefix)/opt/python/libexec/bin"
-    fish_add_path --prepend --move "$(brew --prefix)/opt/python/libexec/bin"
+    # append to avoid shadowing venv python
+    # --path means it's added to system $PATH (not fish_user_path)
+    # since fish_user_path is always prepended
+    fish_add_path --path --prepend --move --global "$(brew --prefix)/opt/python/libexec/bin"
 end
--- shells/fish/conf.d/homebrew_ruby.fish
+++ shells/fish/conf.d/homebrew_ruby.fish
@@ -1,4 +1,4 @@
 if test -d /opt/homebrew/opt/ruby/bin
-    fish_add_path --prepend --move /opt/homebrew/opt/ruby/bin
-    fish_add_path --prepend --move (gem environment gemdir)/bin
+    fish_add_path --prepend --move --global --path /opt/homebrew/opt/ruby/bin
+    fish_add_path --prepend --move --global --path (gem environment gemdir)/bin
 end
