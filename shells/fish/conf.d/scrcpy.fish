4d68bed75c641ca9d322e847ee22604bcd11a903
Matthew Strasiotto <39424834+strazto@users.noreply.github.com>
2026-01-13 15:09:39 +1100

    feat(scrcpy): add low res default for scrcpy

--- shells/fish/conf.d/scrcpy.fish
+++ shells/fish/conf.d/scrcpy.fish
@@ -2,4 +2,9 @@ if ! status is-interactive
     exit
 end
 
-abbr --add android scrcpy --stay-awake --turn-screen-off
+abbr --add android-hq scrcpy --stay-awake --turn-screen-off
+
+abbr --add android \
+    scrcpy \
+        --stay-awake --turn-screen-off \
+        --max-size 1024 --video-bit-rate 4M --max-fps 30 --no-audio
