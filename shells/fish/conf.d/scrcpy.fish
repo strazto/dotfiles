732493b8578defafe1679a9c822e89917ed648eb
Matthew Strasiotto <39424834+strazto@users.noreply.github.com>
2026-01-22 13:50:14 +1100

    feat(scrcpy): remove turn-screen-off

--- shells/fish/conf.d/scrcpy.fish
+++ shells/fish/conf.d/scrcpy.fish
@@ -2,9 +2,9 @@ if ! status is-interactive
     exit
 end
 
-abbr --add android-hq scrcpy --stay-awake --turn-screen-off
+abbr --add android-hq scrcpy --stay-awake
 
 abbr --add android \
     scrcpy \
-        --stay-awake --turn-screen-off \
+        --stay-awake \
         --max-size 1024 --video-bit-rate 4M --max-fps 30 --no-audio
